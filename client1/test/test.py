ac2c3ec59aeb46e869ad1fc6eff2c8182b27d098
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-14 12:53:20 +0530

    minimize the size

--- client1/test/test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-current_date = datetime.date.today()
-print(current_date)
-import datetime
-
-current_month = datetime.datetime.now().month
-print(current_month)
--- client3/encodeParameter.py
+++ client3/encodeParameter.py
@@ -4,34 +4,33 @@ import base64
 import modelGenerator as mg
 import modelAccuracy as ma
 import dataSetSplit as sp
-import pickle
 import os
-
+import zlib
+import numpy as np
+import io
 
 
 def encodeModelParameters():
-    # x_train_np, y_train_np,x_test_np,y_test_np =sp.splitDataset()
-
+   
     print("Encoding ----------------> ")
     model = mg.create_model()
     model.load_weights('backup/model_weights.h5')
-     # Get the size of the saved model weight file
-    # model_size_bytes = os.path.getsize('modelData/model_weights.h5')
-    # # Convert bytes to MB
-    # model_size_mb = model_size_bytes / (1024 * 1024)
 
-    # print(f"The size of the  model parameters is {model_size_mb:.2f} MB.")
-    # ma.getModelAccuracy(model,x_test_np,y_test_np)
-    receivedModelParameters  = model.get_weights()
-    
-    #encode the model
-    model_by= pickle.dumps(receivedModelParameters)
-    encoded_message =base64.b16encode(model_by)
-    #convert to string
-    my_string = encoded_message.decode("utf-8")
+    model1_weights = model.get_weights()
 
-    # print(type(my_string))
+    # save the model weights to an in-memory buffer
+    buf = io.BytesIO()
+    np.savez(buf, *model1_weights)
+    model_bytes = buf.getvalue()
+
+    # compress the serialized weights
+    compressed_model = zlib.compress(model_bytes)
+
+    # encode the compressed data as a base64 string
+    my_string = base64.b64encode(compressed_model).decode('utf-8')
+
     print("Size of encoded model parameter is (String Data type): {:.2f} MB".format(len(my_string) / (1024 * 1024)))
+    print(type(my_string))
 
     # print("Return encoded parameters as string")
     return my_string
@@ -42,26 +41,19 @@ def decodeModelParameters(encoded_message):
     directory = "receivedModelParameter" #replace with  directory path
     num_files = len([f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))])
     num_files =num_files+1
-    # x_train_np, y_train_np,x_test_np,y_test_np =sp.splitDataset()
 
-    #decode the model
-    # print(type(encoded_message))
-    my_bytes = encoded_message.encode("utf-8")
-    # print(type(my_bytes))
-    decode_b64 = base64.b16decode(my_bytes)
-    decode_model_weights=pickle.loads(decode_b64)
-   
+    # decode the base64 string and decompress the data
+    compressed_model = base64.b64decode(encoded_message.encode('utf-8'))
+    model_bytes = zlib.decompress(compressed_model)
+
+    # load the model parameters from the serialized data
+    with np.load(io.BytesIO(model_bytes)) as data:
+        model_weights = [data[f'arr_{i}'] for i in range(len(data.files))]
+
     model = mg.create_model()
-    model.set_weights(decode_model_weights)
-    # ma.getModelAccuracy(model,x_test_np,y_test_np)
+    # set the model parameters to the loaded values
+    model.set_weights(model_weights)
     model.save_weights(f'receivedModelParameter/model_weights_{num_files}.h5')
     print(f'Decode completed and save Received model parameter {num_files}')
-    return decode_model_weights
-
-
-
-# #encode the model weights 
-# encodedData = encodeModelParameters()
-# #decode the model weights
-# decodeModelParameters(encodedData)
+    return model_weights
 
