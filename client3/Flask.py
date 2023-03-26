cd59dc68b2bbd8783abe8731a6a39c617940fa1a
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-26 21:04:41 +0530

    add

--- client3/Flask.py
+++ client3/Flask.py
@@ -6,13 +6,16 @@ import writeFile as wf
 import QRScanner
 import datetime
 import client 
+import modelAccuracy as getThreand
 
 selectedItem ="Item 0"
 ItemListArray = [];
 totalBill = 0
-threandingArray=[["https://aldprdproductimages.azureedge.net/media/resized/$Aldi_GB/19.05.22/4088600260457_0_XL.jpg" , "https://5.imimg.com/data5/ANDROID/Default/2020/10/YU/QD/UL/35343054/prod-20201011-0159397534769397062872599-jpg-500x500.jpg" , "https://food.fnr.sndimg.com/content/dam/images/food/products/2020/1/7/rx_vegetable-goldfish-sweet-carrot.jpg.rend.hgtvcom.616.616.suffix/1578432241151.jpeg" , "https://i0.wp.com/priyadi.lk/wp-content/uploads/2022/06/easy_day_porridge-1.png?fit=230%2C356&ssl=1"],
+threandingArray=[["https://aldprdproductimages.azureedge.net/media/resized/$Aldi_GB/19.05.22/4088600260457_0_XL.jpg" , "https://www.bigbasket.com/media/uploads/p/xxl/20004325_10-uncle-chips-uncle-chips-potato-chips-plain-salted-flavour.jpg" , 
+                  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/813xqlCcX6S._SL1500_.jpg" ,
+                  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/813axPlVxBL.jpg"],
                  ["https://cdn.takas.lk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/0/2/02_1.png" , "https://5.imimg.com/data5/ANDROID/Default/2020/10/YU/QD/UL/35343054/prod-20201011-0159397534769397062872599-jpg-500x500.jpg" , "https://food.fnr.sndimg.com/content/dam/images/food/products/2020/1/7/rx_vegetable-goldfish-sweet-carrot.jpg.rend.hgtvcom.616.616.suffix/1578432241151.jpeg" , "https://i0.wp.com/priyadi.lk/wp-content/uploads/2022/06/easy_day_porridge-1.png?fit=230%2C356&ssl=1"],
-                 ["https://aldprdproductimages.azureedge.net/media/resized/$Aldi_GB/19.05.22/4088600260457_0_XL.jpg" , "https://5.imimg.com/data5/ANDROID/Default/2020/10/YU/QD/UL/35343054/prod-20201011-0159397534769397062872599-jpg-500x500.jpg" , "https://food.fnr.sndimg.com/content/dam/images/food/products/2020/1/7/rx_vegetable-goldfish-sweet-carrot.jpg.rend.hgtvcom.616.616.suffix/1578432241151.jpeg" , "https://i0.wp.com/priyadi.lk/wp-content/uploads/2022/06/easy_day_porridge-1.png?fit=230%2C356&ssl=1"],
+                 ["https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/813axPlVxBL.jpg" , "https://images.albertsons-media.com/is/image/ABS/960123886-A1C1?$ng-ecom-pdp-mobile$&defaultImage=Not_Available" , "https://food.fnr.sndimg.com/content/dam/images/food/products/2020/1/7/rx_vegetable-goldfish-sweet-carrot.jpg.rend.hgtvcom.616.616.suffix/1578432241151.jpeg" , "https://www.onlinekade.lk/wp-content/uploads/2021/10/8901491101844-300x300.jpg"],
                  ["https://aldprdproductimages.azureedge.net/media/resized/$Aldi_GB/19.05.22/4088600260457_0_XL.jpg" , "https://5.imimg.com/data5/ANDROID/Default/2020/10/YU/QD/UL/35343054/prod-20201011-0159397534769397062872599-jpg-500x500.jpg" , "https://food.fnr.sndimg.com/content/dam/images/food/products/2020/1/7/rx_vegetable-goldfish-sweet-carrot.jpg.rend.hgtvcom.616.616.suffix/1578432241151.jpeg" , "https://i0.wp.com/priyadi.lk/wp-content/uploads/2022/06/easy_day_porridge-1.png?fit=230%2C356&ssl=1"],
                  ["https://aldprdproductimages.azureedge.net/media/resized/$Aldi_GB/19.05.22/4088600260457_0_XL.jpg" , "https://5.imimg.com/data5/ANDROID/Default/2020/10/YU/QD/UL/35343054/prod-20201011-0159397534769397062872599-jpg-500x500.jpg" , "https://food.fnr.sndimg.com/content/dam/images/food/products/2020/1/7/rx_vegetable-goldfish-sweet-carrot.jpg.rend.hgtvcom.616.616.suffix/1578432241151.jpeg" , "https://i0.wp.com/priyadi.lk/wp-content/uploads/2022/06/easy_day_porridge-1.png?fit=230%2C356&ssl=1"],
                  ["https://aldprdproductimages.azureedge.net/media/resized/$Aldi_GB/19.05.22/4088600260457_0_XL.jpg" , "https://5.imimg.com/data5/ANDROID/Default/2020/10/YU/QD/UL/35343054/prod-20201011-0159397534769397062872599-jpg-500x500.jpg" , "https://food.fnr.sndimg.com/content/dam/images/food/products/2020/1/7/rx_vegetable-goldfish-sweet-carrot.jpg.rend.hgtvcom.616.616.suffix/1578432241151.jpeg" , "https://i0.wp.com/priyadi.lk/wp-content/uploads/2022/06/easy_day_porridge-1.png?fit=230%2C356&ssl=1"]
@@ -26,7 +29,13 @@ def findCurrentThreandArray():
     global currentThreandArray
    
     global threandingArray
-    currentThreandArray.append(threandingArray[1])
+    #get current threand
+    month = datetime.datetime.now().month
+    gender = 0
+    itemNum =  getThreand.getCurrentThreand(month,gender)
+    print(month,gender)
+    print(itemNum)
+    currentThreandArray.append(threandingArray[itemNum])
 
     
 @app.route('/')
@@ -112,6 +121,6 @@ def flask_thread():
     app.run()
 
 if __name__ == '__main__':
-    t = Thread(target=app.run, kwargs={'port': 5003})
+    t = Thread(target=app.run, kwargs={'port': 5001})
     t.start()
     # client.backgroudNetworkProcess()
