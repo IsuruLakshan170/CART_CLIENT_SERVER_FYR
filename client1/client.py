caf6bf12fbd8c685dd2abc15a3263de2be283cb0
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-28 22:36:03 +0530

    save

--- client1/client.py
+++ client1/client.py
@@ -120,13 +120,16 @@ from enumList import conctionType
 from com import communicationProx
 from seed import seedProx
 from file import getID
+import Main 
+import encodeParameter
+import pandas as pd
 
 HOST = '141.145.200.6' #141.145.200.6
 PORT = 9000
 
 ########################################################################
 #------------------------------PEER   DATA-----------------------------#
-MODELPARAMETERS  = "jhjhhkhkhkl"
+MODELPARAMETERS = encodeParameter.encodeModelParameters()
 # MODELPARAMETERS  = bytes(1024)  # 1 KB
 # MODELPARAMETERS  = bytes(100*1024)  # 100 KB
 # MODELPARAMETERS  = bytes(1024*1024)  # 1 MB
@@ -160,17 +163,81 @@ def mainFunn(MODE, TIMEOUT = 12, RECIVER_TIMEOUT = 5*60, SYNC_CONST = 1):
         print("USER TYPE  : ",MODE)
         if MODE == conctionType.KERNEL.value:
             MODELPARAMETERLIST = communicationProx(mySocket,TEMPUSERID,MODE,RECIVER_TIMEOUT,MODELPARAMETERS,USERID)
+            print("LIST")
+            print("length : ",len(MODELPARAMETERLIST))
+            for item in MODELPARAMETERLIST:
+                if "MODELPARAMETERS" in item['Data']:
+                    receivedData = item['Data'][1]
+                    # print(receivedData)
+                    encodeParameter.decodeModelParameters(receivedData)
+                    
         if MODE == conctionType.SHELL.value:
             seedProx(mySocket,TEMPUSERID,MODE,MOBILEMODELPARAMETERS,MODELPARAMETERS,RECIVER_TIMEOUT,USERID)
+    
     except Exception as e:
         print("Error occurred while running in", MODE, " mode ")
 
-#Call from Separat thread
-if __name__ == "__main__":
-    while True:
-        randomNo= random.randint(0, 50)
-        if randomNo > 15:
-            mainFunn("SHELL")
-        else:
-            mainFunn("KERNEL")
-        time.sleep(randomNo)
+# #Call from Separat thread
+# if __name__ == "__main__":
+#     while True:
+#         randomNo= random.randint(0, 50)
+#         if randomNo > 15:
+#             mainFunn("SHELL")
+#         else:
+#             mainFunn("KERNEL")
+#         time.sleep(randomNo)
+
+
+    
+def connectNetwork(type):
+    if type == "SHELL":
+            mainFunn("SHELL",50)
+            time.sleep(2)
+            print("loop call triggered")
+
+    elif type == "KERNEL":
+            mainFunn("KERNEL",15)
+            time.sleep(2)
+            print("loop call triggered")
+
+
+#----------------------background process --------------------------------
+def backgroudNetworkProcess():
+      while True:
+            directoryModelData = "backup" 
+            modelDataSize = len([f for f in os.listdir(directoryModelData) if os.path.isfile(os.path.join(directoryModelData, f))])
+            cartData = pd.read_csv('dataset/cartData.csv')
+            #if cart is new 
+            if modelDataSize == 0:
+                 print("Initializing cart")
+                 while True:
+                    directoryReceivedParameters = "receivedModelParameter" 
+                    receivedParametersSize = len([f for f in os.listdir(directoryReceivedParameters) if os.path.isfile(os.path.join(directoryReceivedParameters, f))])
+                    #check received parameters size 
+                    if receivedParametersSize >= 4:
+                        Main.initialAggregationProcess()
+                        break
+                    else:
+                        connectNetwork("KERNEL") 
+            
+                
+            #compare size of the dataset for globla aggregation
+            elif len(cartData) >= 3:
+                print("Connecting as KERNEL for globla aggregation")
+                while True:
+                    directoryReceivedParameters = "receivedModelParameter" 
+                    receivedParametersSize = len([f for f in os.listdir(directoryReceivedParameters) if os.path.isfile(os.path.join(directoryReceivedParameters, f))])
+                    #check received parameters size 
+                    if receivedParametersSize >= 4:
+                        Main.globleAggregationProcess()
+                        break
+                    else:
+                        connectNetwork("KERNEL") 
+            
+            else:
+                print("Connecting as SHELL for send Models")
+                connectNetwork("SHELL")
+            
+            time.sleep(5)  
+        
+
