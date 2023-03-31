b4cdd8b04dd58d8dfc77121fc1c524d4b7c30c19
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2023-03-31 15:26:29 +0530

    add db

--- clientOriginalCopy/test.py
+++
@@ -1,4 +0,0 @@
-import client
-
-print("test")
-
--- clientOriginalCopy/util.py
+++
@@ -1,8 +0,0 @@
-from enumList import peerType
-
-def requestModel(msgFrom, data,msgTo = peerType.SERVER.value):
-    return {
-        'Sender':msgFrom,
-        'Receiver': msgTo,
-        'Data':data
-    }
